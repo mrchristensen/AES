a230d7cb4ed2a2e4d4708c8c01717be0e0bbd1fe
Matt <mr.christensen@gmail.com>
2021-09-09 14:06:17 -0600

    Refactored unit test structure

--- tests.py
+++ tests.py
@@ -2,7 +2,11 @@ import unittest
 from main import xtime
 
 
-class AESTests(unittest.TestCase):
+class TestAES(unittest.TestCase):
+    pass
+
+
+class Testxtime(TestAES):
     def test_xtime_no_mod(self):
         self.assertEqual(xtime(0x57), 0xae)
 
