d87c81a29c7b6de12e2c0e66049d1c204360636a
Matt <mr.christensen@gmail.com>
2021-09-12 15:20:41 -0600

    Refactored tests into groups (FF Arithmetic and Key Expansion)

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class TestAES(unittest.TestCase):
     pass
 
 
-class Test_xtime(TestAES):
+class TestFiniteFieldArithmetic(TestAES):
     def test_xtime_no_mod(self):
         self.assertEqual(xtime(0x57), 0xae)
 
@@ -21,13 +21,12 @@ class Test_xtime(TestAES):
         self.assertEqual(xtime(0x47), 0x8e)
         self.assertEqual(xtime(0x8e), 0x07)
 
-
-class Test_ff_mult(TestAES):
     def test_ff_mult(self):
         self.assertEqual(ff_mult(0x57, 0x13), 0xfe)  # from spec
 
-class Test_sub_word(TestAES):
-    def test_ff_from_spec(self):
+
+class TestKeyExpansion(TestAES):
+    def test_sub_word(self):
         self.assertEqual(sub_word(0x00102030), 0x63cab704)
         self.assertEqual(sub_word(0x40506070), 0x0953d051)
         self.assertEqual(sub_word(0x8090a0b0), 0xcd60e0e7)
